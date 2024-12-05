fc7ecb8815b2528580c12c98aeed13887a9fc5f0
Vova <85615105+vova0020@users.noreply.github.com>
2024-12-05 14:50:27 +0300

    1

--- client/src/components/routeList.tsx
+++ client/src/components/routeList.tsx
@@ -1,4 +1,7 @@
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
 import React from 'react'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 // type Props = {}  {}: Props 
 
@@ -12,40 +15,61 @@ export default function RouteList() {
         </div>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-          <h3 style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%' }}>Артикул детали</h3>
-          <h3 style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%' }}>Полуфабрикат</h3>
+          <h3 style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px' }}>Артикул детали</h3>
+          <h3 style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px' }}>Полуфабрикат</h3>
         </div>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px', width: '100%' }}>
 
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginBottom: '20px', width: '65%' }}>
 
-            <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%', marginBottom: '20px' }}>
-              <h3>Модель___</h3>
+            <div style={{ border: '1px solid black', padding: ' 10px', borderRadius: '5px', marginBottom: '20px' }}>
+              <h3>Модель:</h3>
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-              <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%', width: '150px' }}>Поддон №</div>
-              <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%', width: '150px' }}>Дата</div>
-              <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%', width: '150px' }}>Кол-во</div>
+              <div style={{ border: '1px solid black', padding: ' 10px', borderRadius: '5px', width: '150px', marginRight: '5px' }}>Поддон №:
+                <span style={{ fontWeight: 'bold', marginLeft: '5px' }}>
+                  1
+                </span>
+              </div>
+              <div style={{ border: '1px solid black', padding: ' 10px', borderRadius: '5px', width: '150px', marginRight: '5px' }}>Дата:
+                <span style={{ fontWeight: 'bold', marginLeft: '5px' }}>
+                  15.11.2024
+                </span>
+              </div>
+              <div style={{ border: '1px solid black', padding: ' 10px', borderRadius: '5px', width: '150px' }}>Кол-во:
+                <span style={{ fontWeight: 'bold', marginLeft: '5px' }}>
+                  100
+                </span>
+              </div>
             </div>
 
-            <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%' }}>
-              <h3>Материал</h3>
+            <div style={{ border: '1px solid black', padding: ' 10px', borderRadius: '5px' }}>
+              <h3>Материал: </h3>
             </div>
 
           </div>
 
-          <div >
-            <h2>
+          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px', width: '30%', height: '30%' }} >
+            {/* <h2>
               QR код
-            </h2>
+            </h2> */}
+            <img style={{ width: '100%', height: '100%' }} src="https://avatars.mds.yandex.net/i?id=4db0bb030753bb8510d0967e006ede91_l-5234784-images-thumbs&n=13" alt="" />
           </div>
 
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%' }}>Размер по раскрою</div>
-          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%' }}>Размер готовой детали</div>
+          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px' }}>Размер по раскрою:
+            <span style={{ fontWeight: 'bold', marginLeft: '5px' }}>
+            400x222
+            </span>
+          </div>
+          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px' }}>Размер готовой детали:
+            <span style={{ fontWeight: 'bold', marginLeft: '5px' }}>
+            400x222
+            </span>
+          </div>
         </div>
       </div>
 
@@ -53,23 +77,102 @@ export default function RouteList() {
 
         <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
 
-          <div style={{ width: '45%', height:'400px' }}>
+          <div style={{ width: '55%', height: '400px' }}>
 
             <div>
-              <h4>Деталей в заказе</h4>
+              <h4>Деталей в заказе: </h4>
             </div>
 
-            <div>
-              <div style={{ border: '1px solid black', padding: ' 20px',  width: '60%', height:'100px' }}>
+            <div style={{ width: '100%', border: '1px solid black', borderRadius: '5px', padding: ' 20px' }}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {/* Верхняя область */}
+                <div style={{ width: '200px', height: '40px', border: '1px dashed black', marginBottom: '5px', textAlign: 'center' }}>
+                  Название кромки
+                </div>
+
+                {/* Основной контейнер */}
+
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  {/* Левая боковая часть */}
+                  <div style={{ width: '40px', height: '200px', border: '1px dashed black', marginRight: '20px', writingMode: 'vertical-rl', textAlign: 'center', transform: ' rotate(180deg)', padding: '5px' }}>
+                    Название кромки
+                  </div>
+
+                  {/* Центральный прямоугольник */}
+                  <div>
+                    <div style={{ width: '200px', height: '100px', border: '1px solid black', position: 'relative', top: '25%', textAlign: 'center' }}>
+                      <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                        400 мм x 222 мм
+                      </span>
+                      <div style={{ position: 'absolute', top: '-15px', left: '50%', transform: 'translateX(-50%)', fontSize: '12px' }}>
+                        2 мм
+                      </div>
+                      <div style={{ position: 'absolute', bottom: '-15px', left: '50%', transform: 'translateX(-50%)', fontSize: '12px' }}>
+                        0.8 мм
+                      </div>
+                      <div style={{ position: 'absolute', top: '50%', left: '-15px', transform: 'translateY(-50%) rotate(180deg)', fontSize: '12px', writingMode: 'vertical-lr' }}>
+                        0.4 мм
+                      </div>
+                      <div style={{ position: 'absolute', top: '50%', right: '-15px', transform: 'translateY(-50%) rotate(180deg)', fontSize: '12px', writingMode: 'vertical-lr' }}>
+                        0.4 мм
+                      </div>
+                    </div>
+                  </div>
+
+
+                  {/* Правая боковая часть */}
+                  <div style={{ width: '40px', height: '200px', border: '1px dashed black', marginLeft: '20px', writingMode: 'vertical-rl', textAlign: 'center', transform: 'translateY(0%) rotate(180deg)', padding: '5px' }}>
+                    Название кромки
+                  </div>
+                </div>
+
+                {/* Нижняя область */}
+                <div style={{ width: '200px', height: '40px', border: '1px dashed black', marginTop: '5px', textAlign: 'center' }}>
+                  Название кромки
+                </div>
+
+                {/* Поле "Паз" */}
+                <div style={{ marginTop: '10px', textAlign: 'center' }}>
+                  Паз: _______________________
+                </div>
+                <div style={{ marginTop: '10px', textAlign: 'center' }}>
+                  <Button variant="contained">Открыть чертеж</Button>
+                </div>
 
               </div>
             </div>
           </div>
 
-          <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5%', width: '35%', height:'400px' }}>
-            Маршрутная таблица
+          <div style={{ width: '40%' }}>
+            <div>
+              <h4 style={{ textAlign: 'center' }}>Маршрутная таблица </h4>
+            </div>
+            <div style={{ border: '1px solid black', padding: ' 20px', borderRadius: '5px', width: '100%', height: '400px' }}>
+              <div style={{ display: 'flex' }}>
+                <p>Раскрой</p>
+                <Checkbox
+                  defaultChecked
+                  disabled
+                  sx={{
+                    color: 'green', // Цвет чекбокса
+                    '&.Mui-disabled': {
+                      color: 'green', // Цвет для неактивного состояния
+                    },
+                  }}
+                />
+              </div>
+              <div style={{ display: 'flex' }}>
+                <p>Присадка</p>
+                <Checkbox disabled />
+
+              </div>
+            </div>
+
           </div>
 
+        </div>
+        <div style={{ width: '90%', textAlign: 'center', border: '1px solid black', padding: ' 5px', borderRadius: '10px' }}>
+          <h2>Название и номер заказа </h2>
         </div>
 
       </div>
--- client/src/pages/operators/operatorPages.tsx
+++ client/src/pages/operators/operatorPages.tsx
@@ -3,31 +3,62 @@ import TableOperators from './components/tableOperators';
 import StatInfo from './components/statInfo';
 import PaddonsTable from './components/paddonsTable';
 import RouteList from '../../components/routeList';
-import { Drawer, List, ListItem, ListItemText } from '@mui/material';
-
-
+import { Button, Drawer, List, ListItem, ListItemText, Typography } from '@mui/material';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 
 
 export default function OperatorPages() {
-   
+
 
     return (
         <div style={{ width: '100%', padding: '10px' }}>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-                <div></div>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems:'center' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', padding:'10px' }}>
+                    {/* <div> */}
+                    <div style={{  padding:'10px', marginRight:'50px' }}>
+                        <Button
+                            variant="contained"
+                            style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
+                            color="error"
+                        >
+                            Открыть журнал
+                            {"\n"}заказа отбракованных  {"\n"} деталей
+                        </Button>
+                    </div>
+                    <div style={{  padding:'10px' }}>
+                        <Button
+                            variant="contained"
+                            style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
+                            color="success"
+                        >
+                            <PlayCircleIcon />
+                            Запустить
+                        </Button>
+                    </div>
+                    <div style={{  padding:'10px' }}>
+                        <Button
+                            variant="contained"
+                            color="error"
+                        >
+                            Сломался <CancelIcon/> 
+                        </Button>
+                    </div>
+                    {/* </div> */}
+                </div>
                 <div style={{ width: '35%' }}><StatInfo /></div>
             </div>
             <div style={{ width: '100%', marginTop: '10px', }}>
                 <TableOperators />
             </div>
-            
-            <div style={{ width: '100%', marginTop: '10px', }}>
+
+            {/* <div style={{ width: '100%', marginTop: '10px', }}>
                 
-            </div>
-            <div style={{ width: '100%', marginTop: '10px', }}>
-                {/* <RouteList /> */}
-            </div>
+            </div> */}
+            {/* <div style={{ width: '100%', marginTop: '10px', }}>
+                <RouteList />
+            </div> */}
         </div>
     );
 }
